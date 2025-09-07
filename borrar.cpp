2ecfc5bcf271e719e1051a8c65a31d0e415f9e54
AugustoUrquizu <augusto.urquizu@gmail.com>
2025-09-07 20:54:59 -0300

    what

---
+++ borrar.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int borrar(const string &s, int n, vector<vector<int>> &dp){
+    //Si el substring es de largo 1, necesito solo una eliminacion
+    for(int i; i<n; i++) dp[i][i]=1;
+    //Lleno una tabla que posea las eliminaciones necesarias para cada substring con longitud l
+    for(int l=2; l<=n; l++){
+    int i=0;
+        //Lleno dp con las eliminaciones necesarias para eliminar los substring s[i:j] 
+        for(int j=l-1; j<n; j++){
+            dp[i][j]= dp[i][j-1]+1;
+
+            //Exploro intervalo s[i:j] y veo si puedo juntar caracteres para reducir las eliminaciones
+            for(int k=i; k<j; k++){
+                if(s[j]==s[k]){
+                    if(k+1<=j-1){
+                    //Tomo minimo de que si busco juntar los caracteres entre k y j o no y tomo el mejor caso
+                    dp[i][j]=min(dp[i][j], dp[i][k]+dp[k+1][j-1]); 
+                    }
+                    else dp[i][j]=dp[i][k]; //j se elimina junto a k
+                }
+            }
+            i++;
+        }
+}
+    return dp[0][n-1];
+}
+
+int main(){
+    int n;
+    string s;
+    cin >> n >> s;
+    vector<vector<int>> dp(n, vector<int>(n, 0));
+    cout << borrar(s,n, dp);
+    return 0;
+}
--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,1 +1,46 @@
-actual+1
+#include <iostream>
+#include <unordered_map>
+using namespace std;
+
+int borrar(string s, int n){
+    //memo que guarda un caracter y el caracter que aparece despues de este en el string
+    unordered_map <char, char> memo;
+    int i=0;
+    int res=0;
+    while(i<n-1){
+        char actual=s[i];
+        char siguiente=s[i+1];
+
+        memo[actual]=siguiente;
+        //Para tener menos borrados tengo que borrar caracteres que se encuentran entre un mismo caracter
+        // Ej: azza(z se encuentra entre a asi que lo elimino)
+        if(actual!=siguiente && memo.count(siguiente) ){
+
+            //Elimino de memo los caracteres que estan entre en medio de un caracter y actualizo el res
+            //Ej : azbca(zbc entre a asi que los elimino de memo)
+            char eliminar=memo[siguiente];
+            while(eliminar!=siguiente){
+                res++;
+                char siguiente_eliminado=memo[eliminar];
+                memo.erase(eliminar);
+                eliminar=siguiente_eliminado;
+            }
+        }
+        i++;
+    }
+    char actual=s[i];
+
+    //Si el ultimo caracter actual esta en memo, sera eliminado en cadena cuando elimine a los otros iguales
+    //Ej: abzaa (el ultimo a se elimina cuando elimino los otros a)
+    if(!memo.count(actual)){
+        return res+= memo.size()+1;
+    }
+    return res+= memo.size();
+}
+
+int main(){
+    int n;
+    string s;
+    cin >> n >> s;
+    cout << borrar(s,n);
+}
