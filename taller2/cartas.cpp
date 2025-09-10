5483a4a6f83b02d723aea808f5617eaaac2f9c69
AugustoUrquizu <augusto.urquizu@gmail.com>
2025-09-10 02:02:49 -0300

    retoques

--- taller2/cartas.cpp
+++ taller2/cartas.cpp
@@ -14,13 +14,13 @@ int frecuencia(int k, vector<int> lista){
     return frec;
 }
 
-int alegria_carta(int k,int n, vector<int> alegria, vector<vector<int>> dp){
+int alegria_carta(int k,int n, const vector<int> &alegria, vector<vector<int>> &dp){
     //Entrego las cartas 1 persona a la vez
     for(int j=0; j<=n; j++){
         //Entrego las cartas 1 por 1 a la persona actual
         for(int i=0; i<=k; i++ ){
             //Entrego las cartas una por una (ignoro caso i=t ya que entrego 0 cartas)
-            for(int t;t<=i; t++){
+            for(int t=0;t<=i; t++){
                 //Busco cuantas cartas me conviene entregarle a la persona j para maximizar la alegria
                 dp[i][j]=max(dp[i][j], dp[i-t][j-1]+alegria[t]);
                 }   
@@ -30,7 +30,7 @@ int alegria_carta(int k,int n, vector<int> alegria, vector<vector<int>> dp){
         return dp[k][n];
         }
 
-int alegria_total(int k, vector<int> cartas, vector<int> favoritos, vector<int> alegria, int n){
+int alegria_total(int k, const vector<int> &cartas, const vector<int> &favoritos, const vector<int> &alegria, int n){
     //Matriz de k+1xn+1 en la que guardare la alegria de entregar k cartas a n personas
     vector<vector<int>> dp(k+1, vector<int>(n+1,0));
     unordered_map<int,tuple<int,int>> frec;
@@ -47,28 +47,33 @@ int alegria_total(int k, vector<int> cartas, vector<int> favoritos, vector<int> 
     //Si una persona no tiene una carta como favorito, da igual si evaluo alegria con esa carta en esa persona
     for (auto i = frec.begin(); i != frec.end(); i++){
         tuple<int,int> par_frecc= i->second;
-        int k= get<0>(par_frecc);
-        int n= get<1>(par_frecc);
+        int cant_carta= get<0>(par_frecc);
+        int cant_fav= get<1>(par_frecc);
         res+=alegria_carta(k, n, alegria, dp);
     }
+    return res;
 }
 
-vector<int> crear_vector(int n) {
-    vector<int> v(n+1);
-    for (int i = 0; i < n; i++) {
+void llenar_vector(vector<int> &v, int comienzo=0) {
+    for (int i = comienzo; i < v.size(); i++) {
         cin >> v[i];
     }
-    return v;
 }
 
 int main(){
     int n,k;
     cin >> n >>k;
-    vector<int> cartas=crear_vector(n*k);
-    vector<int> favoritos= crear_vector(n);
-    vector<int> alegria= crear_vector(k);
-    //h0=0
-    alegria.insert(alegria.begin(),0);
 
+    vector<int> cartas(n*k);
+    llenar_vector(cartas);
+
+    vector<int> favoritos(n);
+    llenar_vector(favoritos);
+    
+    vector<int> alegria(k+1);
+    //Tengo en cuenta alegria base
+    alegria[0]=0;
+    llenar_vector(alegria,1);
+    
     cout << alegria_total(k, cartas, favoritos, alegria, n);   
 }
