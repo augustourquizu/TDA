86e435816bcafb05c0d7949008f8ffea254bc0ab
AugustoUrquizu <augusto.urquizu@gmail.com>
2025-10-30 12:55:09 -0300

    listo

--- taller3/arbol_unico_v2.py
+++ taller3/arbol_unico_v2.py
@@ -1,5 +1,4 @@
 import sys
-from collections import defaultdict
 input=sys.stdin.readline
 
 class DSU:
@@ -13,12 +12,6 @@ class DSU:
             self.parent[v] = self.parent[self.parent[v]]
             v=self.parent[v] 
         return v
-    
-    def se_pueden_unir(self, v, w):
-        raiz_v=self.find(v)
-        raiz_w=self.find(w)
-        
-        return raiz_v!=raiz_w
     
     def union(self, v,w):
         
@@ -48,37 +41,37 @@ def generar_grafo():
 
 ady,n = generar_grafo()
 
+def agrupar_pesos():
+    i=0
+    grupos={}
+    while i<len(ady):
+        p, v, w =ady[i]
+        while i<len(ady) and ady[i][0]==p:
+            v,w=ady[i][1],ady[i][2]
+            grupos.setdefault(p,[]).append((v,w))
+            i+=1
+    return grupos
+
 def kruskal():
     ady.sort()
     dsu=DSU(n)
-    #Agrupo las aristas por pesos y agrupo las aristas por pesos en un mismo AGM arbitrario
-    agm_original_pesos={}
-    agrupar_pesos=defaultdict(list)
-    #Los pesos de las aristas en el AGM
-    pesos_agm=set()
-    for p,v,w in ady:
-        if dsu.union(v,w):
-            agm_original_pesos.setdefault(p, []).append((v, w))
-            pesos_agm.add(p)
-        else:
-            if p in pesos_agm:
-                agrupar_pesos[p].append((v,w))
-    
-    return agm_original_pesos, agrupar_pesos
-
-def agregar_pesos():
-    agm_original_pesos, agrupar_pesos=kruskal()
-    dsu=DSU(n)
+    grupos=agrupar_pesos()
     res=0
-    #Prueba a anadir cada arista y ver si genera un AGM
-    for p, m in agrupar_pesos.items():
-        #Evaluo si hubiese podido agregar otras aristas de las que agrege en el AGM original
-        for v,w in m:
-            if dsu.se_pueden_unir(v,w):
-                res+=1
-        for v,w in agm_original_pesos[p]:
-            dsu.union(v,w) 
+    for aristas in grupos.values():
+        candidatos=[]
+        for v,w in aristas:
+            raiz_v=dsu.find(v)
+            raiz_w=dsu.find(w)
+            if raiz_v!=raiz_w:
+                candidatos.append((v,w))
+        agregados=0
+        for v, w in candidatos:
+            if dsu.union(v,w):
+                #Armo un AGM arbitrario
+                agregados+=1
+        #Solo tengo que aumentar una vez el peso de una arista para descartarla
+        #Elimino aristas hasta que mi AGM arbitrario sea unico
+        res+=len(candidatos)-agregados
     return res
-    
-        
-print(agregar_pesos())
+
+print(kruskal())
